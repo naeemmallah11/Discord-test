56e30461952f3c360f26951b55e7d6003e542ac9
ikashifraza <141767314+ikashifraza@users.noreply.github.com>
2024-04-08 18:10:27 +0500

    pair.js

--- modules/commands/pair.js
+++ modules/commands/pair.js
@@ -1,9 +1,14 @@
+/**
+* @author Zeeshan Altaf
+* @warn Do not edit code or edit credits
+* @Dont Change This Credits Otherwisw Your Bot Lol
+*/
 module.exports.config = {
   name: "pair",
   version: "1.0.0", 
   hasPermssion: 0,
-  credits: "Anup Kumar",
-  description: "Pairing",
+  credits: "Zeeshan Altaf ",
+  description: "Dont Chang This Credits Otherwise Your Bot Lol",
   commandCategory: "Love", 
   usages: "pair", 
   cooldowns: 40
@@ -27,7 +32,7 @@ module.exports.run = async function({ api, event,Threads, Users }) {
         let Avatar = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data; 
             fs.writeFileSync( __dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8") );
 
-        let gifLove = (await axios.get( `https://i.ibb.co/wC2JJBb/trai-tim-lap-lanh.gif`, { responseType: "arraybuffer" } )).data; 
+        let gifLove = (await axios.get( `https://i.ibb.co/F5t2HNn/romantic.gif`, { responseType: "arraybuffer" } )).data; 
             fs.writeFileSync( __dirname + "/cache/giflove.png", Buffer.from(gifLove, "utf-8") );
 
         let Avatar2 = (await axios.get( `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
@@ -39,6 +44,9 @@ module.exports.run = async function({ api, event,Threads, Users }) {
               imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
               imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
 
-        var msg = {body: `🥰Successful pairing!\n💌Wish you two hundred years of happiness\n💕Double ratio: ${tle}%\n`+namee+" "+"💓"+" "+name, mentions: arraytag, attachment: imglove}
+        var msg = {body: `❰Cϴ𝝢Ĝ𝙍Uɭ𝘼Ƭĭϴ𝝢𝘚❱
+\n🫀Sada kush rehyao yehe dua hai humyre🙈
+\n𝙃𝙀𝘼𝙍𝙏𝘽𝙀𝘼𝙏 𝘾𝙊𝙐𝙉𝙏 ❣︎ ${tle}%
+\n`+namee+" "+"💝"+" "+name, mentions: arraytag, attachment: imglove}
         return api.sendMessage(msg, event.threadID, event.messageID)
-  }
+}
