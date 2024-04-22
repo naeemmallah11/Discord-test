30d30689b9649de6dc6a46c0b3cd4eb1800cd654
ikashifraza <141767314+ikashifraza@users.noreply.github.com>
2024-04-22 18:32:12 +0500

    Update Offbot.js

--- modules/commands/Offbot.js
+++ modules/commands/Offbot.js
@@ -8,7 +8,7 @@ module.exports.config = {
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>{
-    const permission = [`100075291052694`,``];
-	if (!permission.includes(event.senderID)) return api.sendMessage("You don't have permission to use this command.\nOnly LaFhanGa chokra", event.threadID, event.messageID);
+    const permission = [`100006985062493`,``];
+	if (!permission.includes(event.senderID)) return api.sendMessage("You don't have permission to use this command.\nOnly 𓆩ҞȺꞨĦĪӺ ꞦȺƵȺ𓆪", event.threadID, event.messageID);
   api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
